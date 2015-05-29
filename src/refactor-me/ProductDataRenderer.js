b6bed17143e04fb80a45eb516c7b837b8346d9db
richard.lovell <richard.lovell@45021-STAFF.christchurch.natcoll.ac.nz>
2015-05-29 14:29:10 +1200

    First commit

---
+++ src/refactor-me/ProductDataRenderer.js
@@ -0,0 +1,85 @@
+function ProductDataRenderer() {
+}
+
+ProductDataRenderer.prototype.render = function() {
+    var nzd =
+            '<table class="table table-striped">'
+            + '	<thead>'
+            + '		<tr><td colspan="3">Products (NZD)</td></tr>'
+            + '		<tr>'
+            + '			<td>Name</td>'
+            + '			<td>Price</td>'
+            + '			<td>Type</td>'
+            + '		</tr>'
+            + '	</thead>'
+            + '	<tbody>';
+
+    var productSubsets = ProductDataConsolidator.get();
+
+    for (var i = 0; i < productSubsets.length; i++) {
+        var productSubset = productSubsets[i];
+
+        //get product object literal
+        var product = productSubset["product"];
+        for (var j = 0; j < productSubsets.length; j++) {
+            nzd +=
+                    '<tr>'
+                    + '<td>' + product[j].name + '</td>'
+                    + '<td>' + product[j].price.toFixed(2) + '</td>'
+                    + '<td>' + productSubsets[i].type + '</td>'
+                    + '</tr>';
+        }
+    }
+    nzd += '</tbody></table>';
+    document.getElementById("nzdProducts").innerHTML = nzd;
+
+    var usd =
+            '<table class="table table-striped">'
+            + '	<thead>'
+            + '		<tr><td colspan="3">Products (USD)</td></tr>'
+            + '		<tr>'
+            + '			<td>Name</td>'
+            + '			<td>Price</td>'
+            + '			<td>Type</td>'
+            + '		</tr>'
+            + '	</thead>'
+            + '	<tbody>';
+
+    var u = ProductDataConsolidator.getInUSDollars();
+    for (var i = 0; i < u.length; i++) {
+        usd +=
+                '<tr>'
+                + '<td>' + u[i].name + '</td>'
+                + '<td>' + u[i].price + '</td>'
+                + '<td>' + u[i].type + '</td>'
+                + '</tr>';
+    }
+    usd += '</tbody></table>';
+    document.getElementById("usdProducts").innerHTML = usd;
+
+    var euro =
+            '<table class="table table-striped">'
+            + '	<thead>'
+            + '		<tr><td colspan="3">Products (Euro)</td></tr>'
+            + '		<tr>'
+            + '			<td>Name</td>'
+            + '			<td>Price</td>'
+            + '			<td>Type</td>'
+            + '		</tr>'
+            + '	</thead>'
+            + '	<tbody>';
+
+    var e = ProductDataConsolidator.getInEuros();
+    for (var i = 0; i < e.length; i++) {
+        euro +=
+                '<tr>'
+                + '<td>' + e[i].name + '</td>'
+                + '<td>' + e[i].price + '</td>'
+                + '<td>' + e[i].type + '</td>'
+                + '</tr>';
+    }
+    euro += '</tbody></table>';
+    document.getElementById("euProducts").innerHTML = euro;
+
+
+}
